a126fef10fb86fce96aedcdae9a5c9414cfec1b6
MGrigoriev <MGrigoriev@8b57d82a-e0d7-11dd-9d8c-a70aa0a93a20>
2009-02-10 18:41:01 +0000

    change link to admin-panel: /admin/

--- trunk/opentodo/urls.py
+++ trunk/opentodo/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns('',
     (r'^opentodo/', include('todo.urls')),
     (r'^opentodo/logout/$', 'django.contrib.auth.views.logout_then_login'),
     (r'^opentodo/accounts/login/$', 'django.contrib.auth.views.login'),
-    (r'^opentodo/admin/(.*)', admin.site.root),
+    (r'^admin/(.*)', admin.site.root),
 )
