2fe26a7368daceb896c0d41f5a149bdea16dfb80
MGrigoriev <MGrigoriev@8b57d82a-e0d7-11dd-9d8c-a70aa0a93a20>
2009-02-17 13:08:28 +0000

    template modifications for demo - if host is 'opentodo.openminds.ru'

--- opentodo/context_processors.py
+++ opentodo/context_processors.py
@@ -1,6 +1,6 @@
 def site_media_url(request):
     from django.conf import settings
-    return {'MEDIA_URL': settings.MEDIA_URL,}
+    return {'MEDIA_URL': settings.MEDIA_URL, 'HOST': request.get_host()}
 #    return {
 #		'SITE_MEDIA_URL': settings.SITE_MEDIA_URL,
 #		'PROJECT_PATH': settings.PROJECT_PATH,
